6aa3210aaa313aa8f3d9602b64558f9fe36f9bcf
davi gerson <dgas1@discente.ifpe.edu.br>
2025-08-05 20:53:04 -0300

    site god of war completo

--- js/main.js
+++ js/main.js
@@ -3,8 +3,25 @@ var swiper = new Swiper(".slide-characters", {
   slidesPerView: 3.5,
   spaceBetween: 19,
   freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+  breakpoints: {
+    //Resolução mobile
+    320: {
+      slidesPerView: 1.1,
+    },
+
+    //Resolução Ipad
+    768: {
+      slidesPerView: 2,
+    },
+
+    //Resolução Tablet
+    991: {
+      slidesPerView: 2.8,
+    },
+
+    //Resolução Default
+    1200: {
+      slidesPerView: 3.5,
+    },
   },
 });
